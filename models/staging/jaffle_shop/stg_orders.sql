9899a082ddcc3943d76f2fc1bf5ace331c833677
Mahaboob Khan Pathan <snowflakelearn101@gmail.com>
2025-04-11 06:01:23 +0000

    Added unit_tests

---
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -0,0 +1,28 @@
+with
+
+orders as ( 
+
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+),
+
+final as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        datediff('day', order_date, {{ dbt.current_timestamp() }}) as days_since_ordered,
+        status like '%pending%' as is_status_pending,
+        case
+            when status like '%return%' then 'returned'
+            when status like '%shipped_pending%' then 'yet_to_be_shipped'
+            when status like '%pending%' then 'placed'
+            else status
+        end as status
+    from
+        orders
+
+)
+
+select * from final
