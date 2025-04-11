754d9abfc6d94d27242bee61aa12edf039e1057f
Mahaboob Khan Pathan <snowflakelearn101@gmail.com>
2025-04-11 06:13:08 +0000

    Added unit_tests for customers

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -13,7 +13,7 @@ customers as (
         id as customer_id,
         last_name as surname,
         first_name as givenname,
-        first_name || ' ' || last_name as full_name
+        initcap(trim(first_name)) || ' ' || initcap(trim(last_name)) as full_name
 
     from
         source
